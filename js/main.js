78c52bb0b968f5371cb112737095182d38e3fc58
Thiago Saife <thiago@farofastudios.com.br>
2015-06-23 12:44:44 -0300

    V 1.2.1

--- js/main.js
+++ js/main.js
@@ -64,11 +64,13 @@ $(document).ready(function () {
 
     $('.blue').click(function () {
         $('.job_ux').fadeTo('slow', 1);
-        $('.job_ilu').fadeTo('slow', 0.2);
-        $('.job_prog').fadeTo('slow', 0.2);
-        $('.job_ux').css('pointer-events', 'all');
-        $('.job_ilu').css('pointer-events', 'none');
-        $('.job_prog').css('pointer-events', 'none');
+//        $('.job_ilu').fadeTo('slow', 0.2);
+//        $('.job_prog').fadeTo('slow', 0.2);
+        $('.job_ilu').css('display', 'none');
+        $('.job_prog').css('display', 'none');
+//        $('.job_ux').css('pointer-events', 'all');
+//        $('.job_ilu').css('pointer-events', 'none');
+//        $('.job_prog').css('pointer-events', 'none');
         if (selected !== 1) {
             $(this).css('background', 'url(image/botao_azul.png) no-repeat center');
             $('.yellow').css('background', 'url(image/botao_desativado.png) no-repeat center');
@@ -78,12 +80,14 @@ $(document).ready(function () {
         inactive();
     });
     $('.yellow').click(function () {
-        $('.job_ux').fadeTo('slow', 0.2);
+//        $('.job_ux').fadeTo('slow', 0.2);
         $('.job_ilu').fadeTo('slow', 1);
-        $('.job_prog').fadeTo('slow', 0.2);
-        $('.job_ux').css('pointer-events', 'none');
-        $('.job_ilu').css('pointer-events', 'all');
-        $('.job_prog').css('pointer-events', 'none');
+//        $('.job_prog').fadeTo('slow', 0.2);
+        $('.job_ux').css('display', 'none');
+        $('.job_prog').css('display', 'none');
+//        $('.job_ux').css('pointer-events', 'none');
+//        $('.job_ilu').css('pointer-events', 'all');
+//        $('.job_prog').css('pointer-events', 'none');
         if (selected !== 2) {
             $(this).css('background', 'url(image/botao_amarelo.png) no-repeat center');
             $('.blue').css('background', 'url(image/botao_desativado.png) no-repeat center');
@@ -93,12 +97,14 @@ $(document).ready(function () {
         inactive();
     });
     $('.purple').click(function () {
-        $('.job_ux').fadeTo('slow', 0.2);
-        $('.job_ilu').fadeTo('slow', 0.2);
+//        $('.job_ux').fadeTo('slow', 0.2);
+//        $('.job_ilu').fadeTo('slow', 0.2);
         $('.job_prog').fadeTo('slow', 1);
-        $('.job_ux').css('pointer-events', 'none');
-        $('.job_ilu').css('pointer-events', 'none');
-        $('.job_prog').css('pointer-events', 'all');
+        $('.job_ux').css('display','none');
+        $('.job_ilu').css('display','none');
+//        $('.job_ux').css('pointer-events', 'none');
+//        $('.job_ilu').css('pointer-events', 'none');
+//        $('.job_prog').css('pointer-events', 'all');
         if (selected !== 3) {
             $(this).css('background', 'url(image/botao_roxo.png) no-repeat center');
             $('.blue').css('background', 'url(image/botao_desativado.png) no-repeat center');
@@ -107,11 +113,11 @@ $(document).ready(function () {
         }
         inactive();
     });
-    
-    $('#submit').mouseover(function(){
+
+    $('#submit').mouseover(function () {
         $(this).css('background', 'url(image/essa_vaga_hover.png) no-repeat center');
     });
-    $('#submit').mouseout(function(){
+    $('#submit').mouseout(function () {
         $(this).css('background', 'url(image/essa_vaga_normal.png) no-repeat center');
     });
 });
@@ -126,16 +132,16 @@ function vagas(name) {
 }
 
 var dictionary = {'jux1': 'Informações mal organizadas te tiram do sério? Então essa vaga pode ser sua. Você precisa saber coordenar projetos, conceituar soluções para problemas de usabilidade, formular e aplicar testes de usabilidade, além de manjar de prototipagem e wireframe. Para juntar-se a nós, é importante ser criativo e ter conhecimento em InDesign, Illustrator e Photoshop. Será um diferencial experiência no mercado de games.',
-                    'jux2':'Se você é organizado, criativo e manja de prototipagem e wireframe, venha para a Farofa Studios. Saber tudo - ou quase - de InDesign, Illustrator e Photoshop é fundamental. Experiência no mercado de games um diferencial.',
-                    'jilu1':'Gosta de games, tem experiência no mercado e sente saudade do Atari? A vaga de Game Designer Pleno da Farofa Studios é perfeita para você. Sua tarefa será criar gamedocs e level design. Criatividade e ser atento aos detalhes também fazem parte da função.',
-                    'jilu2':'Imaginar histórias, inserir obstáculos e criar mundos imaginários faz sua cabeça? Se a resposta for sim, envie seu currículo. Também é necessário ter conhecimentos básicos em Photoshop e Illustrator, além de paixão por jogos, pesquisa e documentação.',
-                    'jilu3':'É criativo e tem noção de design? Gosta de games e produzir o layout de sites perdeu a graça? Envie seu currículo para a vaga de Designer de Interfaces Pleno. É necessário ter experiência no mercado de games e interfaces para web, tablet e mobile. Ter produzido trabalhos voltados ao público infantil será um diferencial.',
-                    'jilu4':'Se você é heavy user de games, tem noções de usabilidade e conhecimentos no Pacote Adobe, esta é sua chance de entrar o time da Farofa Studios.',
-                    'jilu5':'Você adora um storyboard? Então envie seu currículo para a Farofa Studios. Para a função é necessário experiência em ilustração infantil e animação 2D. Produção para mercado de games será um diferencial.',
-                    'jilu6':'Esculpir e dar forma a objetos e personagens é sua cara? Possui conhecimento avançado em modelagem Low Poly para games e personagens animados? Se a resposta é positiva, você pode fazer parte da nossa equipe. Para a vaga é necessário experiência em mapeamento UV, conhecimento básico de Unity3D e funcionamento de Assets, além de habilidade em seguir concepts no processo de modelagem.',
-                    'jilu7':'Se você gosta de brincar de marionete e dar vida aos personagens, seu lugar é aqui. Para a vaga é imprescindível conhecimento profundo de rigging, em conceitos de animação, Unity3D, Mecanim e métodos de exportação em fbx para Unity3D. Experiência no mercado de games e inglês são considerados diferenciais.',
-                    'jpro1':'Gosta de detalhes, tem bom senso e sabe lidar com limitações de software? Então, venha trabalhar conosco. A vaga pede conhecimentos em Unity 3D, C#, experiência em desenvolvimento de jogos 3D e inglês avançado. Contato com arquitetura cliente/servidor, desenvolvimento para plataformas iOS e Android, NGUI e 2dToolkit são diferenciais.',
-                    'jpro2':'Você é da turma dos Jedi ou dos Sith? Na verdade não importa de que lado da Força você está, mas se possui habilidade com Unity 3D, C#, noções de arquitetura cliente/servidor, NGUI e 2dToolkit. Inglês avançado é um diferencial.',
-                    'jpro3':'Banco de dados e Java são seu forte? Tem inglês, experiência com arquitetura cliente/servidor, modelagem de dados, desenvolvimebto back-end para jogos e com Multithreading? Se a resposta for sim, envie seu currículo para a Farofa. Conhecimento em Smartfox, Unity3D e Linux são diferenciais.', 
-                    'jpro4':'Se você manja tudo de programação e gosta de rotina, aqui é seu lugar. Para a vaga é necessário experiência em PHP, Banco de dados SQL, HTML, CSS e Javascript. Diferenciais como experiência com sites responsivos e HTML 5 ganham pontos.'
+    'jux2': 'Se você é organizado, criativo e manja de prototipagem e wireframe, venha para a Farofa Studios. Saber tudo - ou quase - de InDesign, Illustrator e Photoshop é fundamental. Experiência no mercado de games um diferencial.',
+    'jilu1': 'Gosta de games, tem experiência no mercado e sente saudade do Atari? A vaga de Game Designer Pleno da Farofa Studios é perfeita para você. Sua tarefa será criar gamedocs e level design. Criatividade e ser atento aos detalhes também fazem parte da função.',
+    'jilu2': 'Imaginar histórias, inserir obstáculos e criar mundos imaginários faz sua cabeça? Se a resposta for sim, envie seu currículo. Também é necessário ter conhecimentos básicos em Photoshop e Illustrator, além de paixão por jogos, pesquisa e documentação.',
+    'jilu3': 'É criativo e tem noção de design? Gosta de games e produzir o layout de sites perdeu a graça? Envie seu currículo para a vaga de Designer de Interfaces Pleno. É necessário ter experiência no mercado de games e interfaces para web, tablet e mobile. Ter produzido trabalhos voltados ao público infantil será um diferencial.',
+    'jilu4': 'Se você é heavy user de games, tem noções de usabilidade e conhecimentos no Pacote Adobe, esta é sua chance de entrar o time da Farofa Studios.',
+    'jilu5': 'Você adora um storyboard? Então envie seu currículo para a Farofa Studios. Para a função é necessário experiência em ilustração infantil e animação 2D. Produção para mercado de games será um diferencial.',
+    'jilu6': 'Esculpir e dar forma a objetos e personagens é sua cara? Possui conhecimento avançado em modelagem Low Poly para games e personagens animados? Se a resposta é positiva, você pode fazer parte da nossa equipe. Para a vaga é necessário experiência em mapeamento UV, conhecimento básico de Unity3D e funcionamento de Assets, além de habilidade em seguir concepts no processo de modelagem.',
+    'jilu7': 'Se você gosta de brincar de marionete e dar vida aos personagens, seu lugar é aqui. Para a vaga é imprescindível conhecimento profundo de rigging, em conceitos de animação, Unity3D, Mecanim e métodos de exportação em fbx para Unity3D. Experiência no mercado de games e inglês são considerados diferenciais.',
+    'jpro1': 'Gosta de detalhes, tem bom senso e sabe lidar com limitações de software? Então, venha trabalhar conosco. A vaga pede conhecimentos em Unity 3D, C#, experiência em desenvolvimento de jogos 3D e inglês avançado. Contato com arquitetura cliente/servidor, desenvolvimento para plataformas iOS e Android, NGUI e 2dToolkit são diferenciais.',
+    'jpro2': 'Você é da turma dos Jedi ou dos Sith? Na verdade não importa de que lado da Força você está, mas se possui habilidade com Unity 3D, C#, noções de arquitetura cliente/servidor, NGUI e 2dToolkit. Inglês avançado é um diferencial.',
+    'jpro3': 'Banco de dados e Java são seu forte? Tem inglês, experiência com arquitetura cliente/servidor, modelagem de dados, desenvolvimebto back-end para jogos e com Multithreading? Se a resposta for sim, envie seu currículo para a Farofa. Conhecimento em Smartfox, Unity3D e Linux são diferenciais.',
+    'jpro4': 'Se você manja tudo de programação e gosta de rotina, aqui é seu lugar. Para a vaga é necessário experiência em PHP, Banco de dados SQL, HTML, CSS e Javascript. Diferenciais como experiência com sites responsivos e HTML 5 ganham pontos.'
 };
